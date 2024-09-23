aadc9ea98241c21d9b2e735c0e2dfa7bf0873bae
Cristian <cri_1112@hotmail.com>
2024-09-23 10:27:04 -0300

    Datos iniciales

---
+++ SistemaBiblioteca Ago2024/src/sistemaAutogestion/Sistema.java
@@ -0,0 +1,94 @@
+package sistemaAutogestion;
+
+public class Sistema implements IObligatorio {
+
+    @Override
+    public Retorno crearSistemaDeGestion() {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno agregarEstudiante(String nombre, String apellido, int numero) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno obtenerEstudiante(int numero) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno eliminarEstudiante(int numero) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno agregarLibro(String nombre, String ISBN, String categoría, int cantidad) {
+           return Retorno.noImplementada();
+    }
+    
+        @Override
+    public Retorno eliminarLibro(String ISBN) {
+        return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno prestarLibro(String ISBN, int numero) {
+         return Retorno.noImplementada();
+    }
+
+   @Override
+    public Retorno reservarLibro(String ISBN, int numero) {
+         return Retorno.noImplementada();
+    }
+    
+    @Override
+    public Retorno devolverLibro(String ISBN, int numero) {
+         return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarEstudiantes() {
+          return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarLibros() {
+          return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarLibros(String categoria) {
+           return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno listarPrestamos(int numero) {
+           return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno librosMasPrestados() {
+             return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno deshacerEliminaciones(int n) {
+             return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno cantidadPrestamosActivos(String ISBN) {
+           return Retorno.noImplementada();
+    }
+
+    @Override
+    public Retorno prestamosXCategoría() {
+           return Retorno.noImplementada();
+    }
+
+   
+
+
+
+}
---
+++ SistemaBiblioteca Ago2024/test/sistemaAutogestion/IObligatorioTest.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package sistemaAutogestion;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pesce
+ */
+public class IObligatorioTest {
+    
+    private Sistema miSistema;
+    
+    public IObligatorioTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        //Completar para primera entrega
+    }
+
+    @Test
+    public void testCrearSistemaDeGestion() {
+        //Completar para primera entrega
+    }
+    
+    @Test
+    public void testAgregarEstudiante() {
+        //Completar para primera entrega
+    }
+
+    @Test
+    public void testObtenerEstudiante() {
+        //Completar para primera entrega
+    }
+
+    @Test
+    public void testEliminarEstudiante() {
+        //Completar para primera entrega
+    }
+
+    @Test
+    public void testAgregarLibro() {
+        //Completar para primera entrega
+    }
+
+    
+    @Test
+    public void testListarEstudiantes() {
+        //Completar para primera entrega
+    }
+
+       @Test
+    public void testListarLibros() {
+        //Completar para primera entrega
+    }
+    
+         @Test
+    public void testListarLibrosXCategoria() {
+        //Completar para primera entrega
+    }
+    
+    
+}
